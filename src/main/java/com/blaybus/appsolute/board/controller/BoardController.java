dbbc68fecc8120bd16a12f7af3755cd8e8ccfb58
spacedivver <142153611+spacedivver@users.noreply.github.com>
2025-01-12 01:16:55 +0900

    feat: swagger 적용

--- src/main/java/com/blaybus/appsolute/board/controller/BoardController.java
+++ src/main/java/com/blaybus/appsolute/board/controller/BoardController.java
@@ -3,6 +3,8 @@ package com.blaybus.appsolute.board.controller;
 import com.blaybus.appsolute.board.domain.request.BoardRequest;
 import com.blaybus.appsolute.board.domain.response.BoardResponse;
 import com.blaybus.appsolute.board.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +14,22 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/board")
+@Tag(name="게시판 api")
 public class BoardController {
     private final BoardService boardService;
 
+    @Operation(summary="게시판 저장")
     @PostMapping
     public ResponseEntity<String> saveBoard(@RequestBody BoardRequest boardRequest) {
+
         boardService.saveBoard(boardRequest);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "게시판 조회")
     @GetMapping
     public ResponseEntity<List<BoardResponse>> getBoard() {
+
         return ResponseEntity.ok(boardService.getBoard());
     }
 
