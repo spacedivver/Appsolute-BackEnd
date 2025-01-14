bdf5664ca8fbd9b71fe2dc0eb0080d38c639bb25
spacedivver <142153611+spacedivver@users.noreply.github.com>
2025-01-14 16:27:09 +0900

    feat: 리더 컬럼명 수정, 연도 추가, 올해 경험치 반영

--- src/main/java/com/blaybus/appsolute/leaderquest/repository/JpaLeQuestBoardRepository.java
+++ src/main/java/com/blaybus/appsolute/leaderquest/repository/JpaLeQuestBoardRepository.java
@@ -1,6 +1,7 @@
 package com.blaybus.appsolute.leaderquest.repository;
 
 import com.blaybus.appsolute.leaderquest.domain.entity.LeQuestBoard;
+import com.blaybus.appsolute.project.domain.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.Optional;
 public interface JpaLeQuestBoardRepository extends JpaRepository<LeQuestBoard, Long> {
     List<LeQuestBoard> findByUserIdAndMonth(Long userId, Long month);
     Optional<LeQuestBoard> findByUserIdAndLeaderQuestId(Long userId, Long leaderQuestId);
+    List<LeQuestBoard> findByUserId(Long userId);
 }
